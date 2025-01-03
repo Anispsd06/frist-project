88b195ff8969d7b4fd079efa816235cfd46a7dc6
Anispsd06 <anis456anis@gmail.com>
2025-01-04 04:39:19 +0700

    Update database.js

--- backend/configs/database.js
+++ backend/configs/database.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'todolist'           
+    host: 'todo-kbtc-b799.k.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_j8vzGukcx2XbM2gTTgm',
+    database: 'defaultdb',
+    port: '17013',  
 })          // create connection to database
 
 
